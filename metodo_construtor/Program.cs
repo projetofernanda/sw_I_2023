de354df05cacae29d6ac0589a1d385b64f39eacc
projetofernanda <127985948+projetofernanda@users.noreply.github.com>
2023-03-29 14:57:39 -0300

    Add files via upload

---
+++ metodo_construtor/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace metodo_construtor;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Produto p1 = new();
+        //Produto p1 = new Produto("SACI PERERE");
+        Produto p1 = new Produto("ABC", 10);
+
+        p1.MostraDados();
+
+    }
+}
