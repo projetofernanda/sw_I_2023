a1f28c0e50808954e6254ba3e878095f453fb49a
projetofernanda <127985948+projetofernanda@users.noreply.github.com>
2023-03-29 14:29:07 -0300

    Add files via upload

---
+++ explorador/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace explorador;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Produto p1 = new();
+
+        //p1.Nome = "parafuso";
+        p1.AlteraNome("Parafuso");
+        //p1.Preco = 1.67;
+        p1.AlteraPreco(1.98);
+
+        p1.MostraDados();
+
+    }
+}
